92fda5538b0ba5a2f99d4431b1d3840a2fd6a341
Tuan Luong <a6quocoai@gmail.com>
2018-04-19 22:14:59 +0700

    create route tuan

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/tuan')
+def tuan():
+    return render_template('tuan.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
